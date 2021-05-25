938689a588dfafb88447281237a12f424e3f8237
lucasm <lucasrodrigues.mr@gmail.com>
2021-05-25 14:12:03 -0300

    Validação dos Campos

---
+++ js/validacao.js
@@ -0,0 +1,13 @@
+function validar() {
+
+    let nome = document.getElementById("username").value;
+    let email = document.getElementById("email").value;
+
+git 
+    if(nome == ""){
+        alert("Preecha o campo nome!");
+    }else if(email == ""){
+        alert("Preencha o campo email!");
+    }
+}
+
