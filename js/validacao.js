b5dc14e455afdf55dfb08c71a7615192e427eae1
lucasm <lucasrodrigues.mr@gmail.com>
2021-05-25 14:18:24 -0300

    Validação dos Campos

--- js/validacao.js
+++ js/validacao.js
@@ -3,7 +3,7 @@ function validar() {
     let nome = document.getElementById("username").value;
     let email = document.getElementById("email").value;
 
-git 
+
     if(nome == ""){
         alert("Preecha o campo nome!");
     }else if(email == ""){
